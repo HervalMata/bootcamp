3599e4da395c2bb74a7906025304315a8f5e176d
HervalMata <hervalmataamparo@gmail.com>
2020-12-22 20:40:31 -0300

    Configurando Sequelize

---
+++ api/src/config/database.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dialect: 'mysql',
+  host: 'localhost',
+  username: 'root',
+  password: 'math3036',
+  database: 'bootcamp',
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+};
