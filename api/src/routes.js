fc6b84836be3361b8c83accef3cffce21362a8bd
HervalMata <hervalmataamparo@gmail.com>
2020-12-22 23:52:20 -0300

    Cadastro de Usuários

--- api/src/routes.js
+++ api/src/routes.js
@@ -1,15 +1,8 @@
 import { Router } from 'express';
-import User from "./app/models/User";
+import UserController from "./app/controllers/UserController";
 
 const routes = new Router();
 
-routes.get('/', async function (req, res) {
-    const user = await User.create({
-      name: 'Herval Mata',
-      email: 'admin@admin.com',
-      password_hash: '123456'
-    })
-    return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
