c66138171fe2920cd7997787cd023355a5ab1841
HervalMata <hervalmataamparo@gmail.com>
2020-12-24 18:03:46 -0300

    Configurando Rotas

--- web/src/App.js
+++ web/src/App.js
@@ -1,7 +1,14 @@
-import React from 'rwact';
+import React from 'react';
+import history from './services/history';
+import {Router} from 'react-router-dom';
+import Routes from './routes';
 
 function App() {
-  return <div className="App" />;
+  return (
+      <Router history={history}>
+        <Routes />
+      </Router>
+    );
 }
 
 export default App;
