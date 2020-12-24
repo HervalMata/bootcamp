cbb1845da61aa99085f7a24720294c6f08f40f2c
HervalMata <hervalmataamparo@gmail.com>
2020-12-24 20:39:00 -0300

    Rotas Privadas

--- web/src/routes/index.js
+++ web/src/routes/index.js
@@ -1,17 +1,18 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch } from 'react-router-dom';
 import Dashboard from '../pages/Dashboard';
 import Profile from '../pages/Profile';
 import SignUp from '../pages/SignUp';
 import SignIn from '../pages/SignIn';
+import Route from './Route';
 
 export default function Routes() {
   return (
     <Switch>
       <Route path="/" exact component={SignIn} />
       <Route path="/register" component={SignUp} />
-      <Route path="/dashboard" component={Dashboard} />
-      <Route path="/profile" component={Profile} />
+      <Route path="/dashboard" component={Dashboard} isPrivate />
+      <Route path="/profile" component={Profile} isPrivate />
     </Switch>
   );
 }
