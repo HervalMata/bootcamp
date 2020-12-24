ec596b181628ad8a40a38f7dd6bbe56d626fe6e1
HervalMata <hervalmataamparo@gmail.com>
2020-12-24 20:11:45 -0300

    Configurando Reactotron

--- web/src/App.js
+++ web/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import history from './services/history';
 import {Router} from 'react-router-dom';
 import Routes from './routes';
+import './config/ReactotronConfig';
 
 function App() {
   return (
