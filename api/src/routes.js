b5854ad8b842daeac77cf3e740fa38be276e9ae2
HervalMata <hervalmataamparo@gmail.com>
2020-12-23 23:29:11 -0300

    Listando Notificações do Usuário

--- api/src/routes.js
+++ api/src/routes.js
@@ -8,6 +8,7 @@ import ProviderController from "./app/controllers/ProviderController";
 import FileController from "./app/controllers/FileController";
 import AppointmentController from "./app/controllers/AppointmentController";
 import ScheduleController from "./app/controllers/ScheduleController";
+import NotificationController from "./app/controllers/NotificationController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -22,6 +23,7 @@ routes.get('/providers', ProviderController.index);
 routes.get('/appointments', AppointmentController.index);
 routes.post('/appointments', AppointmentController.store);
 routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
 routes.post('/files', upload.single('file'), FileController.store);
 
 export default routes;
