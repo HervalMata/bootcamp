66ade4ae5dc4ac4b55aaeebc0427aba9e3d59984
HervalMata <hervalmataamparo@gmail.com>
2020-12-25 03:00:02 -0300

    Utilizando Unform

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import logo from '~/assets/logo.svg';
-import {Link} from "react-router-dom";
+import {Link} from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 export default function SignIn() {
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
   return (
     <>
       <img src={logo} alt="GoBarber" />
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu email" />
-        <input type="password" placeholder="Sua senha" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
--- web/src/pages/SignUp/index.js
+++ web/src/pages/SignUp/index.js
@@ -1,5 +1,22 @@
 import React from 'react';
+import logo from '~/assets/logo.svg';
+import { Form, Input } from '@rocketseat/unform';
+import { Link } from 'react-router-dom';
 
 export default function SignUp() {
-  return <div />;
+  function handleSubmit(data) {
+    console.tron.log(data);
+  }
+  return (
+    <>
+      <img src={logo} alt="GoBarber" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu email" />
+        <Input name="password" type="password" placeholder="Sua senha" />
+        <button type="submit">Acessar</button>
+        <Link to="/register">JCriar conta gratuita</Link>
+      </Form>
+    </>
+  );
 }
