83739d17c3788c94536315b9deffaf9cd2758486
HervalMata <hervalmataamparo@gmail.com>
2020-12-24 22:55:17 -0300

    Estilos Globais

--- web/src/App.js
+++ web/src/App.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import history from './services/history';
-import {Router} from 'react-router-dom';
+import { Router } from 'react-router-dom';
 import Routes from './routes';
 import './config/ReactotronConfig';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
       <Router history={history}>
         <Routes />
+        <GlobalStyle />
       </Router>
     );
 }
