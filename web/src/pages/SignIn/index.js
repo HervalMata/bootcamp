16ded069eef15d0f41d1cd915aa326442273f9c0
HervalMata <hervalmataamparo@gmail.com>
2020-12-25 16:18:39 -0300

    Autenticação

--- web/src/pages/SignIn/index.js
+++ web/src/pages/SignIn/index.js
@@ -3,6 +3,8 @@ import logo from '~/assets/logo.svg';
 import { Link } from 'react-router-dom';
 import { Form, Input } from '@rocketseat/unform';
 import * as Yup from 'yup';
+import {useDispatch} from "react-redux";
+import {signInRequest} from "~/store/modules/auth/actions";
 
 const schema = Yup.object().shape({
   email: Yup.string()
@@ -13,8 +15,10 @@ const schema = Yup.object().shape({
 
 
 export default function SignIn() {
-  function handleSubmit(data) {
-    console.tron.log(data);
+  const dispatch = useDispatch();
+
+  function handleSubmit({ email, password }) {
+    dispatch(signInRequest(email, password))
   }
   return (
     <>
