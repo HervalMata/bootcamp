8383d0be24e49d6a9ca009542c5c66527f84396a
HervalMata <hervalmataamparo@gmail.com>
2020-12-27 01:31:23 -0300

    Página SignIn

--- mobile/src/pages/SignIn/index.js
+++ mobile/src/pages/SignIn/index.js
@@ -1,18 +1,33 @@
 import React from 'react';
-import { Text } from 'react-native';
+import { Image } from 'react-native';
+import logo from '~/assets/logo.png';
 import Background from '~/components/Background';
-import Input from "~/components/Input";
-import Button from "~/components/Button";
+import {Container, Form, FormInput, SignLink, SignLinkText, SubmitButton} from "./styles";
 
 export default function SignIn() {
   return (
     <Background>
-      <Text>SignIn</Text>
-      <Input
-        style={{ marginTop: 30 }} icon="call"
-        placeholder="Digite seu nome"
-      />
-      <Button>Entrar</Button>
+      <Container>
+        <Image source={logo} />
+        <Form>
+          <FormInput
+            icon="mail-outline"
+            keyboardType="email-address"
+            autoCorrect={false}
+            autoCapitalize="none"
+            placeholder="Digite o seu email"
+          />
+          <FormInput
+            icon="lock-outline"
+            secureTextEntry
+            placeholder="Digite o sua senha"
+          />
+          <SubmitButton onPress={() => {}}>Acessar</SubmitButton>
+        </Form>
+        <SignLink onPress={() => {}}>
+          <SignLinkText>Criar conta gratuita</SignLinkText>
+        </SignLink>
+      </Container>
     </Background>
   );
 }
